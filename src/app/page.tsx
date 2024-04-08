175741049884169af1b135b992b4167963d7bf69
Maciej Kurek <maciejk77@gmail.com>
2024-04-08 21:34:38 +0100

    Fix semantic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <main className="flex flex-col min-h-screen">
         {/* Top component */}
-        <div className="bg-baseblue flex flex-col items-center py-12 text-2xl text-white">
+        <header className="bg-baseblue flex flex-col items-center py-12 text-2xl text-white">
           <Image
             alt="card logos"
             className="m-1"
@@ -20,24 +20,24 @@ export default function Home() {
           <div>
             and for <span className="text-blue-500">free</span>
           </div>
-        </div>
+        </header>
 
-        <div className="bg-baseblue flex justify-center">
+        <section className="bg-baseblue flex justify-center">
           <Image
             alt="hero video poster"
             height={600}
             src="/assets/hero-video-poster-image.jpg"
             width={1200}
           />
-        </div>
+        </section>
 
-        <div className="bg-baseblue flex flex-col justify-around text-white">
-          <div className="m-4 mt-8 flex justify-center">
+        <section className="bg-baseblue flex flex-col justify-around text-white">
+          <article className="m-4 mt-8 flex justify-center">
             <div className="bg-blue-500 px-2 py-2 rounded-full w-32 flex justify-center">
               <div className="text-black">Who are we</div>
             </div>
-          </div>
-          <div className="m-4 flex justify-center">
+          </article>
+          <article className="m-4 flex justify-center">
             <div>
               Our one of a kind integration lorem ipsum dolor sit amet,
               consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -48,10 +48,10 @@ export default function Home() {
               security and Amazon account protection from late deliveries and
               negative feedback, if you ship on time.
             </div>
-          </div>
-        </div>
+          </article>
+        </section>
 
-        <div className="bg-baseblue flex justify-center">
+        <section className="bg-baseblue flex justify-center">
           <div className="mx-auto flex overflow-hidden mb-8">
             <Image
               alt="dogs image"
@@ -82,11 +82,11 @@ export default function Home() {
               width={200}
             />
           </div>
-        </div>
+        </section>
 
         {/* Middle 1 component */}
-        <div className="flex flex-col justify-between px-10 md:px-20">
-          <div className="flex flex-col p-12">
+        <section className="flex flex-col md:flex-row">
+          <article className="flex flex-col p-12">
             <div className="bg-baseblue text-white rounded-full px-4 py-2 w-44 flex justify-center">
               Start saving today
             </div>
@@ -98,8 +98,10 @@ export default function Home() {
               inceptos himenaeos. Aenean vulputate semper sem nec sodales.
               Vestibulum vitae vulputate metus.
             </div>
-            <Button href="#">Start shipping</Button>
-          </div>
+            <Button className="w-72" href="#">
+              Start shipping
+            </Button>
+          </article>
           <Image
             alt="employees high five image"
             className="m-1 p-12"
@@ -107,30 +109,30 @@ export default function Home() {
             src="/assets/feature-image-1.png"
             width={400}
           />
-        </div>
+        </section>
 
         {/* Middle 2 component */}
-        <div className="bg-baseblue flex flex-col md:flex-row items-center text-white py-4">
-          <div className="flex flex-col  md:mx-auto justify-end">
-            <div className="bg-green-200 text-black rounded-full px-4 py-2 w-44 flex justify-center mt-4 mb-8">
+        <section className="bg-baseblue flex flex-col md:flex-row  text-white py-4">
+          <article className="flex flex-col md:mx-auto justify-end">
+            <div className="bg-green-200 text-black rounded-full px-4 py-2 w-44 flex justify-center ml-12 mt-4 mb-8">
               We are a team of
             </div>
-            <div className="font-semibold text-5xl md:text-8xl">
+            <div className="font-semibold text-7xl ml-12 md:text-8xl">
               1,000
               <br /> People
             </div>
-          </div>
+          </article>
           <Image
             alt="dogs bubble image"
-            className="m-1 p-12"
+            className="m-1 mr-12 p-12"
             height={100}
             src="/assets/stats-image.png"
             width={400}
           />
-        </div>
+        </section>
 
         {/* Bottom component */}
-        <div className="flex flex-col justify-between px-10 md:px-12 md:flex-row">
+        <section className="flex flex-col justify-between px-10 md:px-12 md:flex-row">
           <Image
             alt="employees high five image"
             className="m-1 p-12"
@@ -138,7 +140,7 @@ export default function Home() {
             src="/assets/feature-image-2.png"
             width={791 * 0.6}
           />
-          <div className="flex flex-col p-12">
+          <article className="flex flex-col p-12">
             <div className="bg-baseblue text-white rounded-full px-4 py-2 w-44 flex justify-center">
               Start saving today
             </div>
@@ -153,9 +155,11 @@ export default function Home() {
               Anean nec nulla quis ligula venenatis varius. Nunc sapien lacus,
               feugiat nec consecteur a, facilisis et leo. Sed vel.
             </div>
-            <Button href="#">Start shipping</Button>
-          </div>
-        </div>
+            <Button className="w-72" href="#">
+              Start shipping
+            </Button>
+          </article>
+        </section>
       </main>
     </>
   );
